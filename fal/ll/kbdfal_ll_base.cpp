7cf83cf5d6f8a22d5bf116a5f66663c5c6363b9d
Ota Fejfar <ota.fejfar@gmail.com>
2025-09-01 18:00:51 +0200

    chore: Preparing macro KALEIDOSCOPE_ADAPTER_KEYMAP_DEFAULT which needs to be specified on the application level
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

--- fal/ll/kbdfal_ll_base.cpp
+++ fal/ll/kbdfal_ll_base.cpp
@@ -60,56 +60,13 @@
 #include "Radio_manager.h"
 #include "Upgrade.h"
 
-enum
-{
-    QWERTY,
-    NUMPAD,
-    _LAYER_MAX
-}; // layers
+#ifndef KALEIDOSCOPE_ADAPTER_KEYMAP_DEFAULT
+#error "Default Kaleidoscope Adapter Keymap is not specified"
+#endif /* KALEIDOSCOPE_ADAPTER_KEYMAP_DEFAULT */
 
-/*
-    This comments temporarily turns off astyle's indent enforcement so we can make
-    the keymaps actually resemble the physical key layout better
-*/
 KEYMAPS
 (
-    [QWERTY] = KEYMAP_STACKED
-    (
-        /* Left Side */
-        Key_Escape, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6,
-        Key_Tab, Key_Q, Key_W, Key_E, Key_R, Key_T, Consumer_VolumeIncrement,
-        Key_CapsLock, Key_A, Key_S, Key_D, Key_F, Key_G, Consumer_VolumeDecrement,
-        Key_Backslash, Key_Z, Key_X, Key_C, Key_V, Key_B,
-        Key_LeftControl, Key_LeftGui, Key_Backspace, Key_Delete,
-        Key_LeftShift, Key_LeftAlt, Key_Enter, Key_Space,
-
-        /* Right Side */
-        Key_7, Key_8, Key_9, Key_0, Key_Minus, Key_Equals, Key_Backspace,
-        Key_Y, Key_U, Key_I, Key_O, Key_P, Key_LeftBracket, Key_RightBracket,
-        Key_H, Key_J, Key_K, Key_L, Key_Semicolon, Key_Quote, Key_RightShift,
-        Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_RightShift,
-        Key_LEDEffectNext, Key_Home, Key_UpArrow, Key_End,
-        Key_RightArrow, Key_DownArrow, Key_LeftArrow, Key_Enter
-    ),
-
-    [NUMPAD] = KEYMAP_STACKED
-    (
-        /* Left Side */
-        Key_Escape, Key_F1, Key_F2, Key_F3, Key_F4, Key_F5, Key_F6,
-        Key_Tab, Key_NoKey, Key_UpArrow, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
-        Key_CapsLock, Key_LeftArrow, Key_DownArrow, Key_RightArrow, Key_NoKey, Key_NoKey, Key_NoKey,
-        Key_LeftShift, Key_Backslash, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
-        Key_LeftControl, Key_LeftGui, Key_LeftAlt, Key_Space,
-        Key_Space, Key_Backspace, Key_Enter, Key_Delete,
-
-        /* Right Side */
-        Key_F7, Key_F8, Key_F9, Key_F10, Key_F11, Key_F12, Key_Backspace,
-        Key_KeypadSubtract, Key_7, Key_8, Key_9, Key_KeypadDivide, Key_NoKey, Key_Enter,
-        Key_KeypadAdd, Key_4, Key_5, Key_6, Key_KeypadMultiply, Key_NoKey, Key_Backslash,
-        Key_KeypadDot, Key_1, Key_2, Key_3, Key_UpArrow, Key_RightShift,
-        Key_0, Key_Space, Key_LeftArrow, Key_DownArrow,
-        Key_RightArrow, Key_RightControl, Key_Delete, MoveToLayer(QWERTY)
-    )
+    KALEIDOSCOPE_ADAPTER_KEYMAP_DEFAULT
 );
 
 // kaleidoscope::plugin::EEPROMPadding JointPadding(8);
