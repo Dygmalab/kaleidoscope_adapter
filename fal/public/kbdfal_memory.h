346ea5c373774ae4ed4e31967b683f2a44213ba5
Ota Fejfar <ota.fejfar@gmail.com>
2025-12-18 15:23:37 +0100

    refactor: kbdfal_memory - renumbering the KBDMEM Item ids
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

--- fal/public/kbdfal_memory.h
+++ fal/public/kbdfal_memory.h
@@ -28,10 +28,10 @@ extern "C" {
 
     typedef enum
     {
-        KBDMEM_ITEM_TYPE_QUKEYS = 1,
+        KBDMEM_ITEM_TYPE_KEYMAP = 1,
         KBDMEM_ITEM_TYPE_MOUSEKEYS,
-        KBDMEM_ITEM_TYPE_KEYMAP,
         KBDMEM_ITEM_TYPE_KEYROLE,
+        KBDMEM_ITEM_TYPE_QUKEYS,
         KBDMEM_ITEM_TYPE_SUPERKEY,
         KBDMEM_ITEM_TYPE_MACROS,
     } kbdmem_item_type_t;
